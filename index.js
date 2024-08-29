b7934b725299b806785ffbbb9b87934be8e7acdf
James Phillips <jamesphillips052404@gmail.com>
2024-08-29 12:30:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// https://api.openweathermap.org/data/2.5/weather?q={city%20name}&appid={API%20key}
+const apiKey = "395940296cbb9787086185af2204652e";
+
+const text = document.getElementById("input");
+const city = document.getElementById("input");
+const weather = document.getElementById("weather");
+
+async function getWeatherData(){
+    try{
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${text.value}&appid=${apiKey}`);
+    
+        if(!response.ok){
+            throw new Error("Not found");
+        }
+        else{
+            const data = await response.json();
+            displayCityWeather(data);
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+function displayCityWeather(data){
+    console.log(data);
+    weather.innerHTML = `${data.weather[0].description}`;
+}
